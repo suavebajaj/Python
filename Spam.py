38acdf1c76aea7f25cfe9411a2eea284da60d560
Suave Bajaj <bajajsuave@gmail.com>
2020-11-06 05:12:54 +0530

    Added Alert Box

--- Spam.py
+++ Spam.py
@@ -4,3 +4,4 @@ f = open("HelloScript.txt",'r') #Read the Script file/text
 for word in f: #For reading each word in file 
     pyautogui.typewrite(word) #This function will type the characters in the string that is passed
     pyautogui.press("enter") #To press the desired key
+pyautogui.alert(text='Done', title='Sent 100 Hello\'s', button='OK!') #Displays a Alert box when the for loop is closed
