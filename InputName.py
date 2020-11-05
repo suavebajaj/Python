7bfe50010c1f747721716627f514c69ffadfad35
Suave Bajaj <bajajsuave@gmail.com>
2020-11-06 04:45:56 +0530

    Delete InputName.py

--- InputName.py
+++
@@ -1,6 +0,0 @@
-print ("Enter your Name")
-Name = input() #Takes String as an Input 
-print ("\nHowdy " + Name.lower())#Turns the string into lower case
-X1 = ("How are you? ") + Name.upper()#Turns the string into upper case
-print(X1)
-print(X1[8]) #Prints the 8th place string
